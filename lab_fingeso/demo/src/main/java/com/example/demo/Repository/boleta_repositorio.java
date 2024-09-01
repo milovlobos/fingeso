19bf0920c238d2eb53a73c0fb6428a55835d9ef1
Milovan Valenzuela Lobos <111936228+milovlobos@users.noreply.github.com>
2024-08-31 23:45:03 -0400

    Delete lab_fingeso directory

--- lab_fingeso/demo/src/main/java/com/example/demo/Repository/boleta_repositorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Entity.ballot;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface boleta_repositorio extends JpaRepository<ballot, Long> {
-}
